8f116671c977395ab47aeeee36243d454a066714
T O N K L A <jakkitwongtewee@gmail.com>
2023-10-16 21:53:06 +0700

    edit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './pages/Home/Home.js';
 import About from './pages/About/About.js';
 import Contact from './pages/Contact/Contact.js';
+import Skills from './pages/Skills/Skills.js';
 import MasterPage from './components/MasterPage/MasterPage';
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MasterPage><Home /></MasterPage>} />
         <Route path="/about" element={<MasterPage><About /></MasterPage>} />
+        <Route path="/skills" element={<MasterPage><Skills /></MasterPage>} />
         <Route path="/contact" element={<MasterPage><Contact /></MasterPage>} />
       </Routes>
     </Router>
--- src/components/MasterPage/Header.js
+++ src/components/MasterPage/Header.js
@@ -24,6 +24,7 @@ const Header = () => {
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
+        <li><Link to="/skills">Skills</Link></li>
         <li><Link to="/contact">Contact</Link></li>
       </ul>
     </div>
